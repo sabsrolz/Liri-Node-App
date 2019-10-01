b73b76f25784e66a804c91398b59f52a274833b6
sabsrolz <sabsrolz@umich.edu>
2019-10-01 14:43:13 -0500

    initialized variables to take user input

--- liri.js
+++ liri.js
@@ -11,21 +11,18 @@ let Spotify = require("node-spotify-api");
 //initialize key information by importing spotify object from keys JS file
 let spotify = new Spotify(keys.spotify);
 
-spotify.search({ type: "track", query: "All the Small Things" }, function(
-  err,
-  data
-) {
-  if (err) {
-    return console.log("error occured: " + err);
+spotify.search(
+  { type: "track", query: "All the Small Things", limit: 1 },
+  function(err, data) {
+    if (err) {
+      return console.log("error occured: " + err);
+    }
+    const trackObject = JSON.parse(
+      JSON.stringify(data.tracks.items[0].artists)
+    );
+    //console.log(trackObject);
   }
-
-  const trackObject = JSON.parse(JSON.stringify(data.tracks));
-  //console.log(trackObject);
-
-  //console.log(JSON.stringify(data.tracks));
-  //console.log(JSON.parse(JSON.stringify(data.tracks)));
-  //console.log(typeof JSON.stringify(data.tracks));
-});
+);
 
 const axios = require("axios");
 
@@ -35,6 +32,18 @@ axios
     "https://api.seatgeek.com/2/events?performers.slug=JonasBrothers&client_id=Nzk1NDk5M3wxNTY5OTUzMjQ2Ljkz"
   )
   .then(function(response) {
-    console.log("this is the response");
-    console.log(response);
+    //console.log(response);
   });
+
+//OMDb API output
+axios
+  .get("http://www.omdbapi.com/?t=Moana&apikey=trilogy")
+  .then(function(response) {
+    // console.log("movie output starts here");
+    // console.log(response);
+  });
+
+const [, , args] = process.argv;
+console.log(args);
+const type = args.split("-")[0];
+const nameInput = args.split("-")[1];
