ea5f25f45367c4365dd37619fd27b8359b73b7b8
sabsrolz <sabsrolz@umich.edu>
2019-10-01 13:40:56 -0500

    imported necessary modules in js

--- liri.js
+++ liri.js
@@ -1,6 +1,40 @@
 //read and set environmental variables with the dotenv package
 require("dotenv").config();
+
 //import Spotify keys from keys.js file
 let keys = require("./keys.js");
-//initialize key information
-let spotify = new spotify(keys.spotify);
+
+//Spotify API output
+//import Spotify and axios modules
+let Spotify = require("node-spotify-api");
+
+//initialize key information by importing spotify object from keys JS file
+let spotify = new Spotify(keys.spotify);
+
+spotify.search({ type: "track", query: "All the Small Things" }, function(
+  err,
+  data
+) {
+  if (err) {
+    return console.log("error occured: " + err);
+  }
+
+  const trackObject = JSON.parse(JSON.stringify(data.tracks));
+  //console.log(trackObject);
+
+  //console.log(JSON.stringify(data.tracks));
+  //console.log(JSON.parse(JSON.stringify(data.tracks)));
+  //console.log(typeof JSON.stringify(data.tracks));
+});
+
+const axios = require("axios");
+
+//SeatGeek API output
+axios
+  .get(
+    "https://api.seatgeek.com/2/events?performers.slug=JonasBrothers&client_id=Nzk1NDk5M3wxNTY5OTUzMjQ2Ljkz"
+  )
+  .then(function(response) {
+    console.log("this is the response");
+    console.log(response);
+  });
